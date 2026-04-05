2e21644009001cd51f501a58c1b78607e666c3f2
DennisTurco <dennisturco@gmail.com>
2026-04-05 16:38:51 +0200

    fix5

--- scripts/generate-weekly-pin.ts
+++ scripts/generate-weekly-pin.ts
@@ -2,8 +2,8 @@ import { createClient } from "@supabase/supabase-js";
 import { randomInt } from "crypto";
 
 const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_SERVICE_ROLE_KEY!
+  process.env.SUPABASE_URL!,
+  process.env.SUPABASE_SERVICE_ROLE_KEY!
 );
 
 // genera PIN a 4 cifre
